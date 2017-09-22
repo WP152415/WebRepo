a45401f2d1fb69fbb8205441c0f72763b152e6e7
0707 <0707@0707-PC>
2017-09-22 13:50:18 +0900

    최초 생성

---
+++ WebClass/WebContent/js/hello.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $.fn.snow({ minSize: 5, maxSize: 50, newOn: 1000, flakeColor: '#292929' });
+          var val = location.href.substr(
+              location.href.lastIndexOf('=') + 1
+          );
+      });
